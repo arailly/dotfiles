22d7b2ee2ff5e0f5495b26d7649eecb6e1ff0fb9
arailly <yusuke-arai@arai-hara-lab.local>
2019-05-02 17:28:20 +0900

    add multi-term

--- emacs-conf/.emacs.d/init.el
+++ emacs-conf/.emacs.d/init.el
@@ -1,3 +1,28 @@
+;; Register directories as load-path
+(when (< emacs-major-version 23)
+  (defvar user-emacs-directory "~/.emacs.d/"))
+
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory
+	      (expand-file-name (concat user-emacs-directory path))))
+	(add-to-list 'load-path default-directory)
+	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+	    (normal-top-level-add-subdirs-to-load-path))))))
+
+(add-to-load-path "elisp")
+
+;; Hide menu-bar
 (menu-bar-mode -1)
 
+;; Assign C-h as backspace
 (define-key key-translation-map [?\C-h] [?\C-?])
+
+;; Move previous window
+(global-set-key "\C-xp" (lambda () (interactive) (other-window -1)))
+
+;; Use multi-term
+(require 'multi-term)
+(setq multi-term-program shell-file-name)
+(global-set-key (kbd "C-c t") '(lambda () (interactive) (multi-term)))
