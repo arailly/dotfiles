aefe8e20896686117c613bff6236ab2a54d0d60e
arailly <yusuke.arailly@gmail.com>
2019-10-01 14:31:14 +0900

    :bug: fix

--- fish-conf/setup.fish
+++ fish-conf/setup.fish
@@ -4,5 +4,7 @@ if test ! -e $HOME/.config
   mkdir $HOME/.config
 end
 
+rm -rf /home/arai/.config/fish
 ln -sF $HOME/dotfiles/fish-conf/.config/fish $HOME/.config
+rm -rf /home/arai/.config/fisher
 ln -sf $HOME/dotfiles/fish-conf/.config/fisher $HOME/.config
