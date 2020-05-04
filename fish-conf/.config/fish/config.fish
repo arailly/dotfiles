5bc40c8605b34893f14288d4a7d9c8a172a0f512
Yusuke Arai <yusuke.arailly@gmail.com>
2020-05-04 21:56:56 +0900

    :+1: add tex and gcloud path

--- fish-conf/.config/fish/config.fish
+++ fish-conf/.config/fish/config.fish
@@ -1,1 +1,4 @@
-set -x PATH $PATH:$HOME/.nodebrew/current/bin
+set -x PATH $HOME/.nodebrew/current/bin:/usr/local/texlive/2019/bin/x86_64-darwin:$PATH
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/yusuke/workspace/google-cloud-sdk/path.fish.inc' ]; . '/Users/yusuke/workspace/google-cloud-sdk/path.fish.inc'; end
